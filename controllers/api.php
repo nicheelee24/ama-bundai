293000b2df98910e8a4e61b3d70b612b1784cb3a
koiescafe2024 <nicheelee24@gmail.com>
2024-09-15 22:44:58 +0530

    Revert "Update api.php"
    This reverts commit 2278e40bca7865c9b47ed88e33672c2cd6786fec.

--- controllers/api.php
+++ controllers/api.php
@@ -33,7 +33,7 @@ if ($flag == 'createPromotion')//updPromotion
     $name = $_FILES['file']['name'];
     $temp = $_FILES['file']['tmp_name'];
     print_r($_FILES["file"]);
-    die("...");
+    die("..");
     if (move_uploaded_file($temp, "uploads/" . $name)) {
         echo "Your file was uploaded";
     } else {
