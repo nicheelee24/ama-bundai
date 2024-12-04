d73c3f26b25708862a47406902782cce0edb020b
koiescafe2024 <nicheelee24@gmail.com>
2024-12-04 21:42:16 +0530

    Update member-details.php

--- member-details.php
+++ member-details.php
@@ -23,7 +23,7 @@ $query = new MongoDB\Driver\Query($filter, $options);
 $rows = $mongo->executeQuery('gms2024.transactions', $query);
 $transDepositArr = $rows->toArray();
 
-$filter = ['userPhone' => $uphone,'type'=>'withdrawal'];
+$filter = ['userid' => $uid,'type'=>'withdrawal'];
 $options = [];
 $query = new MongoDB\Driver\Query($filter, $options);
 $rows = $mongo->executeQuery('gms2024.transactions', $query);
