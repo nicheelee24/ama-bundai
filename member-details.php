84a56f3414bfe41783d7224a6a836af063ed2fab
koiescafe2024 <nicheelee24@gmail.com>
2024-12-16 21:45:16 +0530

    Update member-details.php

--- member-details.php
+++ member-details.php
@@ -102,7 +102,7 @@ include 'layout/header.php';
             <label style="font-size:18px;">Promotion Applied</label>
           </div>
           <div class="form-group">
-          <label style="font-size:16px;font-weight:normal;color:red">Not Any</label>
+          <label  style="font-size:16px;font-weight:normal;color:red"><?php if($uid!=""){ if($agntsArr[0]->promotionId!='') { echo $agntsArr[0]->promotionId;  ?> <?php }else{echo 'Not Any';}} ?></label>
           </div>
 
           <div class="form-group">
