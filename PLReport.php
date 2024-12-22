60da347f2f740f3635b6d0c7ac15ee4b9c2308aa
koiescafe2024 <nicheelee24@gmail.com>
2024-12-22 14:50:01 +0530

    rep

--- PLReport.php
+++ PLReport.php
@@ -603,7 +603,7 @@ include 'layout/header.php';
                     <th class="lightpurpl">0.00</th>
                   <?php } ?>
 
-                  <th <?php if ($overallCompanyWinLoss > 0){ ?> style="font-weight:bold;text-align:right" <?php } ?>></th><?php echo number_format($overallCompanyWinLoss, 2); ?></th>
+                  <th <?php if ($overallCompanyWinLoss > 0){ ?> style="font-weight:bold;text-align:right" <?php } ?>><?php echo number_format($overallCompanyWinLoss, 2); ?></th>
                   <th style="text-align:right;">0.00</th>
 
                 <?php } ?>
