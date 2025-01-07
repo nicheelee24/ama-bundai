cbd77a1caa0e37c1db1993760ebebfb2d231cb12
koiescafe2024 <nicheelee24@gmail.com>
2025-01-07 17:12:20 +0530

    Update api.php

--- controllers/api.php
+++ controllers/api.php
@@ -359,7 +359,7 @@ if ($flag == 'qrscan') {
     $_SESSION["qrscanned"] = "true";
     $code=$_POST["2fa"];
     $secret = $_SESSION['storedSecrect'];
-    if ($g->checkCode($secret, $code)) {
+    if ($googleAuthenticator->checkCode($secret, $code)) {
     header('Location: ../dashboard.php ', true);
     }
     exit();
