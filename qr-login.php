a546d77aa85ccaea04d596ff391c9118e61a4ab4
koiescafe2024 <nicheelee24@gmail.com>
2025-01-07 23:26:21 +0530

    Update qr-login.php

--- qr-login.php
+++ qr-login.php
@@ -62,6 +62,7 @@
         <div class="input-group mb-3" style="text-align:center">
           <?php 
 if(!isset($_SESSION['qrscanned'])){
+  print_r("qr scanned..".$_SESSION["qrscanned"]);
 ?>
    <scan style="margin-bottom:10px"> Scan QR code using Google authenticator</scan> 
          
